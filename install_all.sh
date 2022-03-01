ae1f1a8089bc66c1049dae730d6f2ab6c3d6f761
Mathias <mathias.de.weerdt@gmail.com>
2022-03-01 15:15:28 +0100

    Updated docker compose reference
    Updated docker compose reference

--- install_all.sh
+++ install_all.sh
@@ -54,7 +54,7 @@ sudo apt update
 sudo usermod -aG docker $(whoami)
 
 # Installing docker-compose from the official repo.
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Running the nodeJs script and installing from the official repo
